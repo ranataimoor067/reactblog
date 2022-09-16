205896b52d1066783bee6521eff86265d23e9ac0
OkenHaha <keithellakpamoken@gmail.com>
2022-09-16 18:29:56 +0530

    New Repo for the Blog cause I had to Refactor the code while making the backend

---
+++ back-end/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8000
+const {MongoClient} = require("mongodb");
+
+
+
+app.use(express.json({extend: false}));
+
+const withDB = async(operations, res) => {
+	try {
+		const client = await MongoClient.connect('mongodb://localhost:27017');
+		const db = client.db("blog");
+		await operations(db);
+		client.close();
+	}
+	catch (error) {
+		res.status(500).json({message: "Error connection to database", error});
+	}	
+}
+
+app.get('/api/articles/:name', async(req, res) => {
+	withDB(async(db) => {
+		const articleName = req.params.name;
+		const articleInfo = await db.collection("articles").findOne({name: articleName})
+		res.status(200).json(articleInfo)
+	}, res)
+});
+
+app.post("/api/articles/:name/add-comments", (req, res) => {
+	const {username, text} = req.body;
+	const articleName = req.params.name;
+	withDB(async(db) => {
+		const articleInfo = await db.collection('articles').findOne({name: articleName})
+		await db.collection('articles').updateOne({name: articleName} , {
+			$set: {
+				coments: articleInfo.comments.concat({username, text}),
+			},
+		}
+	  );
+		const updateArticleInfo = await db.collection('articles').findOne({name: articleName})
+		res.status(200).json(updateArticleInfo)
+	}, res)
+});
+
+app.listen(PORT, () => console.log(`Server started at ${PORT}`));
---
+++ front-end/src/components/AddComment.jsx
@@ -0,0 +1,31 @@
+import React, {useState} from 'react'
+
+const AddComment = (articleName, setArticleInfo) => {
+	const [username, setUsername] = useState('')
+	const [commentText, setcommentText] = useState('')
+	const addComments = async () => {
+		const result = await fetch(`/api/articles/${articleName}/add-comments`, {
+			method: 'post',
+			body: JSON.stringify({username, text: commentText}),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+			const body = await result.json()
+			setArticleInfo(body)
+			setUsername("")
+			setCommentText("")
+	}
+	return (
+		<form className="shadow rounded px-8 pt-6 mb-4">
+			<h3 className="text=xl font-bold mb-4 text-gray-900">Add a comment</h3>
+			<label className="block text-gray-70 text-sm font-bold mb-2">Name: </label>
+			<input type="text" onChange={(e)=> setUsername(e.target.value)} value={username} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
+			<label className="block text-gray-70 text-sm font-bold mb-2">Text: </label>
+			<textarea onChange={(e)=> setcommentText(e.target.value)} value={commentText} rows="4" cols="50" className="block text-gray-70 text-sm font-bold mb-2"/>
+			<button onClick={() => addComments()} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline hover:pointer">Add Coments</button>
+		</form>
+	)
+}
+
+export default AddComment
