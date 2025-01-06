d7e02d39d12e6cb75c279f7812716add705a8896
NOOB-3301 <sepic466@gmail.com>
2025-01-07 01:47:18 +0530

    Added Image in Articles

--- front-end/src/pages/ArticleList.jsx
+++ front-end/src/pages/ArticleList.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Articles from '../components/Articles';
 import AddArticleModal from './AddArticle';
 import { link } from '../components/Baselink';
+import axios from 'axios';
 
 const ArticleList = () => {
   const [articles, setArticles] = useState([]);
@@ -15,13 +16,13 @@ const ArticleList = () => {
     const fetchArticles = async () => {
       try {
         console.log(url)
-        const response = await fetch(`${url}/api/article/getallarticle`);
-        console.log(response)
+        const response = await axios.get(`http://localhost:3000/api/article/getallarticle`);
+        console.log(response.data)
         if (!response.ok) {
-          console.alert("Error while fetching articles");
+          console.log("Error while fetching articles");
         }
-        const data = await response.json();
-        setArticles(data);
+        const data = response.data
+        setArticles(response.data);
       } catch (err) {
         setError(err.message);
       } finally {
