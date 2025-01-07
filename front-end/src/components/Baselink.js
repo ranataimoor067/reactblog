77d86d5a43bcfffcdb5f3e8e7b783457ed289350
NOOB-3301 <sepic466@gmail.com>
2025-01-08 00:43:19 +0530

    fixx.

--- front-end/src/components/Baselink.js
+++ front-end/src/components/Baselink.js
@@ -1,4 +1,3 @@
-// const link = "https://react-blog-server-gamma.vercel.app"
+const link = "https://react-blog-server-gamma.vercel.app"
 // const link = "http://localhost:3000"
-const link = "https://react-blog-f84n.vercel.app"
 export {link}
