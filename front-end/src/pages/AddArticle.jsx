6d73d6078356387d38a3272f7a0cc5b5429e6d62
NOOB-3301 <sepic466@gmail.com>
2025-01-09 21:31:38 +0530

    article list

--- front-end/src/pages/AddArticle.jsx
+++ front-end/src/pages/AddArticle.jsx
@@ -51,7 +51,7 @@ const AddArticleModal = ({ onClose, onSuccess }) => {
 
     return (
         <div className="fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center p-4 transition-opacity duration-300">
-            <div className="bg-gradient-to-r from-indigo-700 to-purple-500 text-white rounded-lg shadow-xl w-full max-w-md transform transition-all animate-fadeIn">
+            <div className="bg-gradient-to-r from-indigo-600 to-purple-500 text-white rounded-lg shadow-xl w-full max-w-md transform transition-all animate-fadeIn">
                 <div className="p-6 space-y-6">
                     <div className="flex items-center justify-between mb-6">
                         <h2 className="text-2xl font-bold text-white">Create a New Article</h2>
