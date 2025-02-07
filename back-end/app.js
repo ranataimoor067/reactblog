0f60ecf4c5af3bdcc10226ab4e3600d5bec1b132
yashpandav <pandavyash076@gmail.com>
2025-02-07 17:03:47 +0530

    create article page ui

--- back-end/app.js
+++ back-end/app.js
@@ -15,9 +15,9 @@ app.use(cors({ origin: '*', credentials: true }));
 
 app.use(express.json({extend: false}));
 
-// app.listen(PORT,()=>{
-//     console.log(`server running on port ${PORT}`)
-// })
+app.listen(8081,()=>{
+    console.log(`server running on port `)
+})
     
 // app.use("/",(req,res)=>{res.send('express running , mongo running')})
 //routes importing
--- front-end/src/components/Baselink.js
+++ front-end/src/components/Baselink.js
@@ -1,5 +1,5 @@
 
-const link = import.meta.env.VITE_LINK || "http://localhost:8080"
+// const link = import.meta.env.VITE_LINK || "http://localhost:8080"
 
-//const link = "http://localhost:8080"
+const link = "http://localhost:8081"
 export {link}
